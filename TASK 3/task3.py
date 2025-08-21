23e0ab9395b5ca841bb79ddbbc30e5229540d3b2
Mithil Dabhi <mithildabhi898@gmail.com>
2025-08-21 15:54:53 +0530

    task 3 complete

--- TASK 3/task3.py
+++ TASK 3/task3.py
@@ -5,15 +5,26 @@ matrix = [
     ["1","0","0","1","0"]
 ]
 
-def compare(matrix):
-    for i in range(9):
-        for j in range(9):
-            if matrix[i][j] == '1':
-                
-            else:    
-                return True  # row, col
-            if matrix[i][j] == matrix[i][j+1]:
-    return None
+def maximalRectangle(matrix):
+    if not matrix:
+        return 0
 
-compare(matrix)
+    n = len(matrix[0])
+    heights = [0] * (n + 1)  # +1 for easier stack handling
+    max_area = 0
 
+    for row in matrix:
+        # update histogram heights
+        for i in range(n):
+            heights[i] = heights[i] + 1 if row[i] == "1" else 0
+        # calculate largest rectangle in histogram
+        stack = [-1]
+        for i in range(n + 1):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                max_area = max(max_area, h * w)
+            stack.append(i)
+    return max_area
+
+print(maximalRectangle(matrix))
